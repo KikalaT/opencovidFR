97ca60bfb8fff29525061d4f0f55b75891c6490e
KikalaT <56830022+KikalaT@users.noreply.github.com>
2021-07-19 12:38:03 +0200

    convert bokeh -> plotly

--- st_app.py
+++ st_app.py
@@ -5,10 +5,6 @@ from io import StringIO
 from datetime import date, timedelta
 import streamlit as st
 import plotly.express as px
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource
-from bokeh.models.tools import HoverTool
-import requests
 
 
 st.title('OPEN COVID (FR) DATASET')
@@ -17,10 +13,10 @@ st.title('OPEN COVID (FR) DATASET')
 ## Données nationales concernant l'épidémie de COVID19
 ### Sources
 
-* Santé publique France - open_file_folder /sante-publique-france
+* Santé publique France
 * Chiffres clés et cas par région
 * Données GÉODES
-* Agences Régionales de Santé - open_file_folder /agences-regionales-sante
+* Agences Régionales de Santé
 * Préfectures
 * Ministère des Solidarités et de la Santé
 * Vidéos / Vidéos en direct
@@ -33,13 +29,18 @@ st.title('OPEN COVID (FR) DATASET')
 """
 
 # Load data
-data = requests.get('https://github.com/opencovid19-fr/data/raw/master/dist/chiffres-cles.csv')
-data = StringIO(data.text)
-df = pd.read_csv(data, sep=",", low_memory=False)
+@st.cache(suppress_st_warning=True)
+def load_data():
+    data = 'https://github.com/opencovid19-fr/data/raw/master/dist/chiffres-cles.csv'
+    df0 = pd.read_csv(data, sep=",", low_memory=False)
+    # format date
+    df0['date'] = df0['date'].str.replace('_','-')
+    df0['date'] = pd.to_datetime(df0['date'], errors='coerce')
+    return df0
 
-# format date
-df['date'] = df['date'].str.replace('_','-')
-df['date'] = pd.to_datetime(df['date']).dt.date
+df = load_data()
+
+
 
 # gps dict
 d_gps = {'France': [46.603354, 1.8883335], 'Charente': [45.6667902, 0.09730504409848517], 'Charente-Maritime': [45.73022675, -0.7212875872563794], 'Corrèze': [45.342904700000005, 1.8176424406120555], 'Creuse': [46.0593485, 2.04890101251091], 'Dordogne': [45.14291985, 0.6321258058651044], 'Gironde': [44.883745950000005, -0.6051264440438711], 'Landes': [44.00996945, -0.6433872354467377], 'Lot-et-Garonne': [44.3691703, 0.45391575832487524], 'Pyrénées-Atlantiques': [43.18718655, -0.728247400084667], 'Deux-Sèvres': [46.53914, -0.29947849341978416], 'Vienne': [46.612116549999996, 0.4654070096639711], 'Haute-Vienne': [45.91901925, 1.203176771876291], 'Île-de-France': [48.6443057, 2.7537863], 'Nouvelle-Aquitaine': [45.4039367, 0.3756199], 'Monde': [44.9863862, 4.5729027], 'Hérault': [43.591422, 3.3553309364095925], 'Haute-Savoie': [46.068820849999994, 6.344536991587102], 'Auvergne-Rhône-Alpes': [45.2968119, 4.6604809], 'Bourgogne-Franche-Comté': [47.0510946, 5.0740568], 'Aisne': [49.453285449999996, 3.606899003594057], 'Doubs': [47.06699155, 6.235622772820445], 'Nord': [50.52896715, 3.0883523694464854], 'Oise': [49.41205455, 2.406487846905477], 'Pas-de-Calais': [50.5144061, 2.258007849773996], 'Somme': [49.96897145, 2.373858954610659], 'Territoire de Belfort': [47.62923095, 6.899301156710882], 'Hauts-de-France': [50.1024606, 2.7247515], 'Grand Est': [48.4845157, 6.113035], "Côte-d'Or": [47.465503350000006, 4.74812234575117], 'Finistère': [48.24511525, -4.044090245241742], 'Loire-Atlantique': [47.34816145, -1.8727461214619257], 'Bas-Rhin': [48.5991783, 7.533818624332648], 'Alpes-Maritimes': [43.9210587, 7.1790785], 'Maine-et-Loire': [47.38863045, -0.3909097146387368], 'Mayenne': [48.1507819, -0.6491273812007092], 'Seine-Maritime': [49.66323745, 0.9401133910609153], 'Guadeloupe': [16.230510250000002, -61.68712602138846], 'Martinique': [14.6367927, -61.01582685063731], 'Guyane': [4.0039882, -52.999998], 'La Réunion': [-21.130737949999997, 55.536480112992315], 'Mayotte': [-12.8253862, 45.148626111147614], 'Centre-Val de Loire': [47.5490251, 1.7324062], 'Normandie': [49.0677708, 0.3138532], 'Pays de la Loire': [47.6594864, -0.8186143], 'Bretagne': [48.2640845, -2.9202408], 'Occitanie': [43.6487851, 2.3435684], "Provence-Alpes-Côte d'Azur": [44.0580563, 6.0638506], 'Corse': [42.188089649999995, 9.068413771427695], 'Ille-et-Vilaine': [48.17276805, -1.6498092420681134], 'Saint-Barthélemy': [17.9036287, -62.811568843006896], 'Saint-Martin': [48.5683066, 6.7539988], 'Morbihan': [47.825981150000004, -2.7633492695588253], 'Sarthe': [48.026928749999996, 0.2538217482247317], 'Ain': [49.453285449999996, 3.606899003594057], 'Ardennes': [49.69801175, 4.671600518245179], 'Aube': [48.3201921, 4.1905396615047525], 'Eure': [49.0756358, 0.9652025944774796], 'Marne': [48.961264, 4.31224359285714], 'Haute-Marne': [48.1329414, 5.252910789751933], 'Meurthe-et-Moselle': [48.95596825, 5.987038299756556], 'Meuse': [49.01296845, 5.428669076639772], 'Moselle': [49.0207259, 6.538035170357949], 'Haut-Rhin': [47.8654746, 7.231543347579764], 'Rhône': [45.8802348, 4.564533629559522], 'Vosges': [48.16378605, 6.382071173595532], 'Allier': [46.36746405, 3.163882848311948], 'Ardèche': [44.815194000000005, 4.3986524702343965], 'Cantal': [45.0497701, 2.699717567737356], 'Drôme': [44.72964575, 5.204559599996514], 'Gard': [43.95995, 4.297637002377168], 'Isère': [45.28979315, 5.634382477386232], 'Loire': [45.75385355, 4.045473682551104], 'Haute-Loire': [45.085724850000005, 3.833826117673291], 'Puy-de-Dôme': [45.7715343, 3.0839934206717934], 'Saône-et-Loire': [46.6557086, 4.55855481835173], 'Savoie': [45.494895150000005, 6.384660381375652], 'Aveyron': [44.315857449999996, 2.5065697302419823], 'Bouches-du-Rhône': [43.5424182, 5.034323560504859], "Côtes-d'Armor": [48.458422150000004, -2.7505868346107736], 'Eure-et-Loir': [48.4474102, 1.3998820185020766], 'Indre-et-Loire': [47.2232046, 0.6866702523286876], 'Haute-Saône': [47.63842335, 6.095114088932768], 'Vaucluse': [43.993864349999996, 5.1818898389002355], 'Hautes-Alpes': [44.6564666, 6.352024584507948], 'Calvados': [49.09076485, -0.24139505722798021], 'Cher': [47.024882399999996, 2.5753333606655704], 'Corse-du-Sud': [41.87340825, 9.0087052196875], 'Haute-Corse': [42.42196975, 9.100906549656115], 'Haute-Garonne': [43.305454600000004, 0.9716791701901577], 'Indre': [46.81210565, 1.5382051557056249], 'Loir-et-Cher': [47.65977515, 1.297183525390464], 'Loiret': [47.9140388, 2.3073794620675887], 'Manche': [49.091895199999996, -1.2454370607545526], 'Paris': [48.8566969, 2.3514616], 'Seine-et-Marne': [48.61902069999999, 3.0418157506708345], 'Yvelines': [48.76203735, 1.8871375621264361], 'Var': [43.4173592, 6.2664620128919], 'Essonne': [48.53034015, 2.239291805668168], 'Hauts-de-Seine': [48.840185899999994, 2.198641221906077], 'Seine-Saint-Denis': [48.9098125, 2.4528634784461856], 'Val-de-Marne': [48.774489349999996, 2.4543321444588204], "Val-d'Oise": [49.07507045, 2.209811443668384], 'Jura': [46.783362499999996, 5.783285726354901], 'Lot': [44.624991800000004, 1.6657742169753669], 'Tarn': [43.7921741, 2.133964772269535], 'Tarn-et-Garonne': [44.080656000000005, 1.2050632958700225], 'Vendée': [46.5040559, -0.7479592], 'Yonne': [47.85512575, 3.6450439257238765], 'Aude': [43.0542733, 2.512471457499548], 'Nièvre': [47.11969705, 3.5448897947227174], 'Orne': [48.57605325, 0.04466171759588161], 'Alpes-de-Haute-Provence': [44.1640832, 6.187851538609079], 'Gers': [43.695527600000005, 0.4101019175237992], 'Polynésie française': [-16.03442485, -146.0490931059517], 'Hautes-Pyrénées': [43.1437925, 0.15866611287926924], 'Pyrénées-Orientales': [42.625894, 2.5065089946931507], 'Lozère': [44.5425706, 3.521114648333333], 'Ariège': [42.9455368, 1.4065544156065486], 'Nouvelle-Calédonie': [-20.454288599999998, 164.55660583077983], 'Wallis et Futuna': [-13.289402, -176.204224]}
@@ -99,29 +100,34 @@ df_P = df_P[(df_P.P == 1) & (df_P.cl_age > 0)]
 
 df_P['cl_age_txt'] = df_P['cl_age'].apply(lambda x:str(x-9)+'-'+str(x))
 
-data0 = ColumnDataSource({'cl_age_txt':df_P['cl_age_txt'].value_counts().index.values,
-							'valeur':df_P['cl_age_txt'].value_counts().values
-							})
-cats = ['0-9','10-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90-99']
-f0 = figure(plot_height=300,x_range=cats)
-g0 = f0.vbar(source=data0,x='cl_age_txt',top='valeur',width=0.3)
-h0 = HoverTool(renderers=[g0],tooltips=[("tranche d'âge",'@cl_age_txt'),('valeur','@valeur')])
-f0.add_tools(h0)
-
-st.bokeh_chart(f0)
-
+fig0 = px.bar(df_P['cl_age_txt'].value_counts())
+st.plotly_chart(fig0)
 
 """
-#### France:Nouvelles hospitalisations
-##### (National)
+#### France : Évolution des 'Nouvelles hospitalisations'
+##### (animation)
 """
+df_anim = df_depts
+df_anim['dt_str'] = df_anim['date'].apply(lambda x: x.strftime("%d-%b-%Y"))
 
-fig1= px.scatter_mapbox(df_depts, lat="lat", lon="lon", size="nouvelles_hospitalisations", hover_name="maille_nom", 
-					zoom=4, center={'lat':48.862725,'lon':2.287592}, height=800,
-					mapbox_style="open-street-map")
+col1, col2 = st.beta_columns(2)
 
-st.plotly_chart(fig1)
-					
+date1 = col1.date_input('Date de début')
+date2 = col2.date_input('Date de fin')
+df_anim = df_anim[(df_anim['date'] >= pd.to_datetime(date1)) & (df_anim['date'] <= pd.to_datetime(date2))]
+
+try:
+	fig1= px.scatter_mapbox(df_anim, lat="lat", lon="lon", 
+						size="nouvelles_hospitalisations", hover_name="maille_nom",
+						animation_frame="dt_str", 
+						zoom=4, center={'lat':48.862725,'lon':2.287592}, 
+						height=800,
+						mapbox_style="open-street-map")
+
+	st.plotly_chart(fig1)
+except KeyError:
+	st.write('Dates incorrectes')
+
 """
 ## Données sur la 'zone' sélectionnée
 """
@@ -137,36 +143,19 @@ df_area = df[df['maille_nom'] == area]
 'Nouvelles hospitalisations', df_area.tail(1)['nouvelles_hospitalisations'].values[0]
 'Nouvelles réanimations', df_area.tail(1)['nouvelles_reanimations'].values[0]
 
-# bokeh charts
+# plotly chart 1
+df_last10 = df_area.tail(10)
+df1 = df_last10.melt(id_vars='date', value_vars=['nouvelles_hospitalisations', 'nouvelles_reanimations'])
+fig1 = px.line(df1, x='date' , y='value' , color='variable')
 
-data = ColumnDataSource(df_area)
-data_last10 = ColumnDataSource(df_area.tail(10))
+# plotly chart 2
+df2 = df_area.melt(id_vars='date', value_vars=['deces','hospitalises'])
+fig2 = px.line(df2, x='date' , y='value' , color='variable')
 
-f1 = figure(plot_width=700,plot_height=400, x_axis_type="datetime")
-r1 = f1.line(source=data_last10,x='date', y='nouvelles_hospitalisations',color='blue', legend_label='Nouvelles Hospitalisations')
-r2 = f1.line(source=data_last10,x='date', y='nouvelles_reanimations', color='green', legend_label='Nouvelles Réanimations')
+# plotly chart 3
+df3 = df_area
+fig3 = px.line(df3, x='date', y='reanimation')
 
-data2 = ColumnDataSource(df_area)
-f2 = figure(plot_width=700,plot_height=400, x_axis_type="datetime")
-r3 = f2.line(source=data2,x='date', y='deces',color='blue', legend_label='Décès')
-r4 = f2.line(source=data2,x='date', y='hospitalises', color='green', legend_label='Hospitalisés')
-
-data3 = ColumnDataSource(df_area)
-f3 = figure(plot_width=700,plot_height=400, x_axis_type="datetime")
-r5 = f3.line(source=data3,x='date', y='reanimation',color='blue', legend_label='Réanimations')
-
-# bokey tools
-h1 = HoverTool(renderers = [r1,r2],tooltips = [("date","@date{%F}"),("valeur","$y{0}")],formatters={"@date":"datetime"})
-h2 = HoverTool(renderers = [r3,r4],tooltips = [("date","@date{%F}"),("valeur","$y{0}")],formatters={"@date":"datetime"})
-h3 = HoverTool(renderers = [r5],tooltips = [("date","@date{%F}"),("valeur","$y{0}")],formatters={"@date":"datetime"})
-
-f1.add_tools(h1)
-f2.add_tools(h2)
-f3.add_tools(h3)
-
-f1.legend.click_policy='hide'
-f2.legend.click_policy='hide'
-f3.legend.click_policy='hide'
 
 """
 #### Les 10 derniers jours (Nouvelles hospitalisations, Nouvelles réanimations) 
@@ -175,7 +164,7 @@ f3.legend.click_policy='hide'
 area
 
 # fig 1 : hosp/reas last10 days
-st.bokeh_chart(f1)
+st.plotly_chart(fig1)
 
 """
 #### Décès, Hospitalisés 
@@ -184,7 +173,7 @@ st.bokeh_chart(f1)
 area
 
 # fig2 : ['deces','hospitalises']
-st.bokeh_chart(f2)
+st.plotly_chart(fig2)
 
 """
 #### Réanimations
@@ -193,4 +182,4 @@ st.bokeh_chart(f2)
 area
 
 # fig3 : ['reanimation']
-st.bokeh_chart(f3)
+st.plotly_chart(fig3)
